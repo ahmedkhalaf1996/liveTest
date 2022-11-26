ce4514f6b1e62f719ad6a77e9a80ed611a5c1ea0
unknown <khalafahmed1996@gmail.com>
2022-11-26 23:26:40 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"video-chat-app/server"
+)
+
+func main() {
+	server.AllRooms.Init()
+
+	fs := http.FileServer(http.Dir("dist"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/create", server.CreateRoomRequestHandler)
+	http.HandleFunc("/join", server.JoinRoomRequestHandler)
+
+	log.Println("Starting Server on Port 8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
